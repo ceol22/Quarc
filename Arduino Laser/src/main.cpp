4b657bb12090171a5a82f43008949b7bc631ca8a
jordywk1507 <jordywk1507@penguin>
2019-11-05 13:50:29 +0100

    Code voor Laser toegevoegd

--- Arduino Laser/src/main.cpp
+++ Arduino Laser/src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
+#include <Servo.h>
+
+#define servoPin 3
+
+int lichtDoelwaarde = 250;   //0-1023
+int servoStart = 0; //0-180
+int servoDoel = 180; //0-180
+
+Servo servo1;
+
 
 void setup() {
-  // put your setup code here, to run once:
+
+  servo1.attach(servoPin);
+  servo1.write(servoStart);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  int lichtWaarde = analogRead(0);
+
+  if(lichtWaarde >= lichtDoelwaarde){
+    servo1.write(servoDoel);
+  }
+
 }
